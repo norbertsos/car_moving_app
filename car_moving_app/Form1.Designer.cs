6d50edb16d18d0fc7fc676680314b3475386dcc5
norbertsos <norbertsos15@gmail.com>
2024-10-07 13:01:59 +0200

    Add project files.

---
+++ car_moving_app/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace car_moving_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.carPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.carPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carPictureBox
+            // 
+            this.carPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.carPictureBox.Location = new System.Drawing.Point(3, 1027);
+            this.carPictureBox.Name = "carPictureBox";
+            this.carPictureBox.Size = new System.Drawing.Size(801, 288);
+            this.carPictureBox.TabIndex = 0;
+            this.carPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1500, 1371);
+            this.Controls.Add(this.carPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.carPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox carPictureBox;
+    }
+}
+
---
+++ car_moving_app/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace car_moving_app
+{
+    public partial class Form1 : Form
+    {
+
+        private Timer timer;
+        private int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+        private int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+        private int currentScreen = 0;
+        private int maxScreens = 12;
+        private int carSpeed = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCarAndTimer();
+            this.FormBorderStyle = FormBorderStyle.None; 
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void InitializeCarAndTimer()
+        {
+            carPictureBox.Image = Image.FromFile("school_bus.png");
+            carPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            timer = new Timer();
+            timer.Interval = 50; 
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            carPictureBox.Left += carSpeed;
+
+            if (carPictureBox.Left >= screenWidth)
+            {
+                currentScreen++;
+                if (currentScreen < maxScreens)
+                {
+                    MoveToNextScreen();
+                }
+                else
+                {
+                    timer.Stop();
+                    MessageBox.Show("Az autó elérte a 12 kijelzőt.");
+                }
+            }
+        }
+
+        private void MoveToNextScreen()
+        {
+            carPictureBox.Left = 0;
+
+            var screen = Screen.AllScreens[currentScreen % Screen.AllScreens.Length];
+            this.Location = screen.Bounds.Location;
+        }
+
+        
+    }
+}
+
